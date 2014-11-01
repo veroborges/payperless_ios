f84bbfc32e008042efe9bffe48759a32cc55ef46
Veronica Borges <borges.vero@gmail.com>
2014-11-01 11:34:09 -0700

    init xcode project

---
+++ Payperless/PayperlessTests/PayperlessTests.swift
@@ -0,0 +1,36 @@
+//
+//  PayperlessTests.swift
+//  PayperlessTests
+//
+//  Created by Veronica Borges on 11/1/14.
+//  Copyright (c) 2014 Veronica Borges. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PayperlessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
